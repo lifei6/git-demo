79021cb49a9f63b544a07ab1137b11f35a9d2c8b
莫若柳絮 <93174383+lifei6@users.noreply.github.com>
2024-06-16 01:47:06 +0800

    Update  utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,10 @@
 console.log("公用代码，utils 被其他人修改并提交到了remote");
+console.log('remote 1111')
 
 function add(num1, num2) {
   return num1 + num2;
+}
+
+function subtract(arg1, arg2) {
+  return arg1 - arg2;
 }
